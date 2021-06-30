ce5e9aac406b2df4d882282d28af298a673ebfd0
Gyu <melon9751@naver.com>
2021-06-30 21:28:28 +0900

    마이페이지 컨트롤러 추가

---
+++ BE/src/main/java/com/dignity/puppymarket/controller/MyPageController.java
@@ -0,0 +1,21 @@
+package com.dignity.puppymarket.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mypage")
+public class MyPageController {
+    private final MyPageService myPageService;
+
+    public MyPageController(MyPageService myPageService) {
+        this.myPageService = myPageService;
+    };
+
+    @GetMapping("/{id}")
+    public MyPageGetResponseDto detail(@PathVariable Long id) {
+        return myPageService.getMyPage(id);
+    }
+}
