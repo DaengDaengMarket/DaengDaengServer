ac647d733069b1552ef74551df8bbc5ef5997a49
Gyu <melon9751@naver.com>
2021-06-30 21:31:20 +0900

    마이페이지 서비스 추가

---
+++ BE/src/main/java/com/dignity/puppymarket/service/MyPageService.java
@@ -0,0 +1,20 @@
+package com.dignity.puppymarket.service;
+
+import com.dignity.puppymarket.error.UserNotFoundException;
+import com.dignity.puppymarket.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyPageService {
+    private final UserRepository userRepository;
+
+    public MyPageService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public MyPageGetResponseDto getMyPage(Long id) {
+        return userRepository.getMyPage(id)
+                .map(MyPageGetResponseDto::of)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+}
