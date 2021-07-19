a60bc92dec2fa6334b3dc5fb498568456b04dd1a
Gyu <melon9751@naver.com>
2021-07-19 15:40:43 +0900

    동네 인증 수정

--- BE/src/main/java/com/dignity/puppymarket/controller/LocationController.java
+++ BE/src/main/java/com/dignity/puppymarket/controller/LocationController.java
@@ -5,7 +5,7 @@ import com.dignity.puppymarket.dto.LocationRequestDto;
 import com.dignity.puppymarket.dto.LocationResponseDto;
 import com.dignity.puppymarket.security.UserAuthentication;
 import com.dignity.puppymarket.service.LocationService;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,10 +26,13 @@ public class LocationController {
     }
 
     @PostMapping("/mypage/{id}/location")
-    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    //@PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     public LocationResponseDto saveLocation(@PathVariable Long id,
                                             @RequestBody LocationCreateRequestDto locationCreateRequestDto,
                                             UserAuthentication userAuthentication) {
+        if (userAuthentication == null) {
+            throw new AccessDeniedException("권한이 없습니다");
+        }
         return locationService.saveLocation(id, locationCreateRequestDto, userAuthentication);
     }
 }
